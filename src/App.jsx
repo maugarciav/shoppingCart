6075b09bc0f0ae4773926e682375ed339fd7b942
maugarciav <mau.villanueva2002@gmail.com>
2024-01-12 11:43:25 -0600

    add: filters into footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,40 @@ import React, { useState } from 'react'
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
-function App () {
-  const [products, setProducts] = useState(initialProducts)
-  const [filters, setFilters] = useState(
-    {
-      category: 'all',
-      minPrice: 0
-    }
+function useFilters () {
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  }
   )
 
   const filterProducts = (products) => {
-    return products.filter(products => {
+    return products.filter(product => {
       return (
-        products.price >= filters.minPrice &&
+        product.price >= filters.minPrice &&
         (
           filters.category === 'all' ||
-          products.category === filters.category
+          product.category === filters.category
         )
       )
     })
   }
 
-  const filterProduct = filterProducts(products)
+  return { filters, filterProducts, setFilters }
+}
+
+function App () {
+  const [products, setProducts] = useState(initialProducts)
+  const { filters, filterProducts, setFilters } = useFilters()
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
       <Header changeFilters={setFilters}/>
-      <Products products={filterProduct}/>
+      <Products products={filteredProducts}/>
+      <Footer filters={filters}/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+import reactLogo from '../assets/react.svg'
+
+export function Footer ({ filters }) {
+  return (
+    <footer className='footer'>
+      {/* <h4>Shopping cart -  <span>@maugarciav</span><img src={reactLogo} alt="React Logo" /></h4>
+      <h5>Shopping cart using useContext and useReducer</h5> */}
+
+      {
+        JSON.stringify(filters, null, 2)
+      }
+    </footer>
+  )
+}
